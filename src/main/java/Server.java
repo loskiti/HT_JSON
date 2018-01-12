ac7352e713b6ce76d4877c2f7f9401669f4fd700
loskiti <loskiti@mail.ru>
2018-01-13 00:28:44 +0300

    Update Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,19 @@
 /**
  * Created by eloseva on 15.12.2017
  */
-public interface Server {
-    void start();
 
+/**
+ * Server interface with HTTP API.
+ */
+public interface Server {
+/**
+ * Bind server to HTTP port and start listening.
+ * May be called only once.
+ */
+    void start();
+/**
+ * Stop listening and free all the resources.
+ * May be called only once and after {@link #start()}.
+ */
     void stop();
 }
