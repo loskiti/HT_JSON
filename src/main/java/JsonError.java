35742a5cea411f0c7894ecb624d5b0b2f84ae358
loskiti <loskiti@mail.ru>
2018-01-12 14:12:16 +0300

    Create JsonError.java

---
+++ src/main/java/JsonError.java
@@ -0,0 +1,23 @@
+/**
+ * Created by eloseva on 12.01.2018
+ */
+ 
+class JsonError {
+
+    private int code;
+    private String message;
+    private String place;
+    private String resource;
+    private int req_id;
+
+    /*
+    * Constructor is fill all fields of error
+    * */
+    JsonError(int code, String message, String place, String resource, int req_id) {
+        this.code = code;
+        this.message = message;
+        this.place = place;
+        this.resource = resource;
+        this.req_id = req_id;
+    }
+}
